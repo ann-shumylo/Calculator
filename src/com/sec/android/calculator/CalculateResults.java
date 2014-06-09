48fc9f09de8ec5fad5072f025e4c034ebaca83e3
Anna <ane4ka@MacBook-Pro-Ane4ka.local>
2014-06-09 17:29:52 +0300

    Deleted ParseMathString class
    Move methods from deleted class to CalculateResults class

--- src/com/sec/android/calculator/CalculateResults.java
+++ src/com/sec/android/calculator/CalculateResults.java
@@ -1,19 +1,22 @@
 package com.sec.android.calculator;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
+
+import static com.sec.android.calculator.OperatorPrecedences.OPERATORS;
 
 public class CalculateResults {
     static double reversePolishNotation(List<String> list) {
         Stack<String> tempStack = new Stack<String>();
 
-        for (String token : ParseMathString.shuntingYardAlgorithm(list)) {
-            if (!ParseMathString.isOperator(token)) {
+        for (String token : shuntingYardAlgorithm(list)) {
+            if (!isOperator(token)) {
                 tempStack.push(token);
             } else {
                 Double num2 = Double.valueOf(tempStack.pop());
                 Double num1 = null;
-                if(!token.equals("s") && !token.equals("c")) {
+                if (!token.equals("s") && !token.equals("c")) {
                     num1 = Double.valueOf(tempStack.pop());
                 }
 
@@ -50,4 +53,44 @@ public class CalculateResults {
         }
         return Double.valueOf(tempStack.pop());
     }
+
+    private static boolean isOperator(String token) {
+        return OPERATORS.containsKey(token);
+    }
+
+    private static List<String> shuntingYardAlgorithm(List<String> list) {
+        List<String> output = new ArrayList<String>();
+        Stack<String> stack = new Stack<String>();
+
+        for (String token : list) {
+            if (isOperator(token)) {
+                while (!stack.empty() && isOperator(stack.peek())) {
+                    if ((OPERATORS.get(token).associativity == OperatorPrecedences.LEFT_ASSOCIATIVITY &&
+                            OPERATORS.get(token).precedence <= OPERATORS.get(stack.peek()).precedence) ||
+                            (OPERATORS.get(token).associativity == OperatorPrecedences.RIGHT_ASSOCIATIVITY &&
+                                    OPERATORS.get(token).precedence < OPERATORS.get(stack.peek()).precedence)) {
+                        output.add(stack.pop());
+                        continue;
+                    }
+                    break;
+                }
+                stack.push(token);
+            } else if (token.equals("(")) {
+                stack.push(token);
+            } else if (token.equals(")")) {
+                while (!stack.empty() && !stack.peek().equals("(")) {
+                    output.add(stack.pop());
+                }
+                stack.pop();
+            } else if (!isOperator(token)) {
+                output.add(token);
+            }
+
+        }
+        while (!stack.empty()) {
+            output.add(stack.pop());
+        }
+        return output;
+    }
+
 }
