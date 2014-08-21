4e42f3b811f296051609c6c5afbddfa9cd8ecef5
Anna <anna.pliskovskaya@gmail.com>
2014-08-21 15:11:21 +0300

    Added styles for keyboard

--- src/com/sec/android/calculator/utils/InputFieldManager.java
+++ src/com/sec/android/calculator/utils/InputFieldManager.java
@@ -1,14 +1,10 @@
 package com.sec.android.calculator.utils;
 
-import java.util.List;
-
 /**
  * @author Ganna Pliskovska(g.pliskovska@samsung.com)
  */
 public interface InputFieldManager {
     void setInputtedSymbol(String str);
-
-
 
     void onEqualClicked();
 }
